cad4889f2dbba6b36436cd0feae0de8a676e1aa9
Cal-Ell <calvin.malagon@ultimagen.com>
2023-05-29 19:44:09 -0700

    template

---
+++ shop/frontend/src/App.js
@@ -0,0 +1,21 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Outlet} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ shop/frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../src/assets/styles/index.css';
+import App from './App';
+import '../src/assets/styles/bootstrap.custom.css';
+import {createRoutesFromElements, RouterProvider, createBrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>} />
+    </Route>
+  )
+)
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
